25df5392d413dc0474ad21b4f54ac237621cc4ce
Anurag Pramanik <p.anurag@outlook.com>
2023-04-27 00:01:21 +0530

    Build category and category card

---
+++ components/Category/Categories.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { ScrollView, Text } from "react-native";
+import CategoryCard from "./CategoryCard";
+
+const Categories = () => {
+  return (
+    <ScrollView
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      contentContainerStyle={{
+        paddingHorizontal: 15,
+        paddingTop: 10,
+      }}
+    >
+      {/* Category card */}
+      <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Test" />
+      <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Test" />
+      <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Test" />
+      <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Test" />
+      <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Test" />
+      <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Test" />
+    </ScrollView>
+  );
+};
+
+export default Categories;
---
+++ components/Category/CategoryCard.tsx
@@ -0,0 +1,23 @@
+import { Text, TouchableOpacity, Image } from "react-native";
+
+type Props = {
+  imgUrl: string;
+  title: string;
+};
+
+const CategoryCard = ({ imgUrl, title }: Props) => {
+  return (
+    <TouchableOpacity className="relative mr-2">
+      <Image
+        source={{
+          uri: imgUrl,
+        }}
+        className="rou h-20 w-20"
+      />
+      <Text className="lef1 absolute bottom-1 font-bold text-white">
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+export default CategoryCard;
