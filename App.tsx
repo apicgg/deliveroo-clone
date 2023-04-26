8c6a828b1c0604a0739404fb726e0904a7c7d2fe
Anurag Pramanik <p.anurag@outlook.com>
2023-04-26 22:31:23 +0530

    feat(deps): add react-navigation and prettier

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-yellow-500">
-        Open up App.tsx to start working on your app!
-      </Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View className="flex-1 items-center justify-center bg-white">
+        <Text className="text-red-500">
+          Open up App.tsx to start working on your app!
+        </Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
